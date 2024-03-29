8b96c21d2c5f615c9bdec892866306da97a953ed
Ogabek2005 <mominovogabek2005@gmail.com>
2024-03-29 20:21:40 +0500

    aa

--- users/models.py
+++ users/models.py
@@ -35,7 +35,7 @@ class Subcategory(models.Model):
 
 class ProductColors(BaseModel):
     name = models.CharField(max_length=50)
-    color = ColorField(default="#FFFFFF")
+    color =ColorField(default="#FFFFFF")
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
@@ -108,7 +108,7 @@ class Location(models.Model):
 
 from admins.models import Center
 
-class ProductClearance(BaseModel):  # Mahsulotni rasmiylashtirish
+class ProductClearance(models.Model):  # Mahsulotni rasmiylashtirish
 
     class CityType(models.Choices):
         TASHKENT = "Toshkent", "toshkent"
@@ -238,8 +238,8 @@ class ProductClearance(BaseModel):  # Mahsulotni rasmiylashtirish
 
 
 
-class UserProduct(BaseModel):
+class UserProduct(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
-    price = MoneyField(quantity, Product.price)
+    price = models.DecimalField(max_digits=13, decimal_places=2)
